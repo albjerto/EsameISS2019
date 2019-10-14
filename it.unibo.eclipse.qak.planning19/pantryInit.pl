09b56f5a67dc15582204090b365f9c9ccaf12bab
imgiohunter <50119974+imgiohunter@users.noreply.github.com>
2019-10-14 20:40:31 +0200

    Update pantryInit.pl

--- it.unibo.eclipse.qak.planning19/pantryInit.pl
+++ it.unibo.eclipse.qak.planning19/pantryInit.pl
@@ -2,7 +2,7 @@
 %% set di piatti, bicchieri e posate inizialmente disponibili nella pantry
 
 tableware(piattoPiano,20)
-tableware(piattoFondo,20)
+tableware(ciotola,20)
 tableware(bicchiere,20)
 tableware(forchetta,20)
 tableware(coltello,20)
@@ -11,19 +11,20 @@ tableare(cucchiaio,20)
 %% controllo presenza tableware con nome T
 isThereTableware(T) :- tableware(T,N), N > 0.
 
-%% ritiro di una quantit‡ N del tableware T
+%% ritiro di una quantit√† N del tableware T
 getTableware(T,N) :- tableware(T,N1), N1 >= N, retract(tableware(T,N1)), N2 is N1 - N, assert(tableware(T,N2)).
 
-%% aggiunta di una quantit‡ N del tableware T
+%% aggiunta di una quantit√† N del tableware T
 putTableware(T,N) :- tableware(T,N1), retract(tableware(T,N1)), N2 is N1 + N, assert(tableware(T,N2)).
 
 %% pone sul tavolo il tableware specificato in prepareTablewareList.pl
 prepareTableware :- tablewareTable(T,N), getTableware(T,N), fail.
 prepareTableware.
 
-%% ripulisce il tavolo dai tableware e li ripone nella pantry
-%% retract non necessaria, avendo gi‡ fatto quella di enabled(true)
-%% direttamente nel codice
+%% rimuove dal tavolo i tableware non usati (puliti) e li ripone nella pantry
+%% retract non necessaria, avendo gi√† fatto quella di enabled(true)
+%% direttamente nel codice, c'√® solo per correttezza semantica
+%% per gestione tableware sporchi vedi dishwasherSupport.pl
 clearTableware :- tablewareTable(T,N), putTableware(T,N), 
 				  retract(tablewareTable(T,N)), fail.
 clearTableware.
