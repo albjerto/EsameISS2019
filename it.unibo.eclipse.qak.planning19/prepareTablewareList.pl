94af39775340f912fa0cfb586ae2266004c07b60
imgiohunter <50119974+imgiohunter@users.noreply.github.com>
2019-10-14 20:43:30 +0200

    Update prepareTablewareList.pl

--- it.unibo.eclipse.qak.planning19/prepareTablewareList.pl
+++ it.unibo.eclipse.qak.planning19/prepareTablewareList.pl
@@ -1,19 +1,41 @@
 %% fatto da noi
 %% tableware da porre sul tavolo col comando prepare
-%% modificare solo le quantit‡ (l'ultimo campo)
+%% modificare solo le quantit√† (l'ultimo campo) di tablewareTable
 %% lasciarlo a 0 se non si vuol prendere il tableware dalla pantry
 %% ma NON cancellare la rule
-%% quantit‡ disponibile di ogni tableware visibile in pantryInit.pl
+%% quantit√† disponibile di ogni tableware visibile in pantryInit.pl
 
-tablewareTable(piattoPiani,5)
-tablewareTable(piattoFondi,5)
-tablewareTable(bicchiere,5)
-tablewareTable(forchetta,5)
-tablewareTable(coltello,5)
-tablewareTable(cucchiaio,5)
+tablewareTable(piattoPiano,5).
+tablewareTable(ciotola,5).
+tablewareTable(bicchiere,5).
+tablewareTable(forchetta,5).
+tablewareTable(coltello,5).
+tablewareTable(cucchiaio,5).
 
-%% print dello stato, restituisce i tableware presenti solo se Ë prima stata 
-%% invocata la prepare, dato che con essa Ë stata fatta l'assert di enabled(true)
+%% ty albi
+corresponding(b01,[bicchiere]).
+corresponding(b02,[bicchiere]).
+corresponding(b03,[bicchiere]).
+corresponding(b04,[bicchiere]).
+corresponding(f01,[ciotola,coltello]).
+corresponding(f02,[ciotola,coltello]).
+corresponding(f03,[ciotola,coltello]).
+corresponding(v01,[ciotola,coltello]).
+corresponding(v02,[ciotola,coltello]).
+corresponding(v03,[ciotola,coltello]).
+corresponding(a01,[piattoPiano,forchetta]).
+corresponding(a02,[piattoPiano,forchetta]).
+corresponding(a03,[piattoPiano,forchetta]).
+corresponding(a04,[piattoPiano,forchetta]).
+corresponding(s01,[piattoPiano,forchetta]).
+corresponding(s02,[piattoPiano,forchetta]).
+corresponding(s03,[piattoPiano,forchetta]).
+corresponding(d01,[piattoPiano,cucchiaio]).
+corresponding(d02,[piattoPiano,cucchiaio]).
+corresponding(d03,[piattoPiano,cucchiaio]).
+
+%% print dello stato, restituisce i tableware presenti solo se √® prima stata 
+%% invocata la prepare, dato che con essa √® stata fatta l'assert di enabled(true)
 %% tolto check N > 0, mostro anche i tableware terminati per chiarezza
 showTablewareTableState :- enabled(true), tablewareTable(T,N), 
 						   outputTablewareTable(tablewareTable(T,N)), fail.
@@ -26,12 +48,41 @@ outputTablewareTable(tablewareTable(T,N)) :- stdout <- print(T),
 getTablewareTableState(L) :- findall([T,N],multipleTablewareGoal(T,N),L).
 multipleTablewareGoal(T,N) :- enabled(true),tablewareTable(T,N).
 
-%% consuma randomicamente una certa quantit‡ di ogni tableware
+%% aggiorna stato del tavolo dopo una add tableware avvenuta con successo
+%% implementata in caso si voglia effettuare il comportamento descritto nel
+%% commento nello state addTable del centralstateserver
+addTablewareTable(T,N) :- tablewareTable(T,N1), retract(tablewareTable(T,N1)), 
+						  N2 is N1 + N, assert(tablewareTable(T,N2)).
+
+%% usata nella correspondingTablewareConsumption
+%% NB i tableware consumati sono quelli che finiranno nella dishwasher quindi
+%% gi√† da ora gestisco anche i tablewareDishwasher in modo che alla clear io
+%% abbia gi√† lo stato della dishwasher creato e debba solo renderlo visibile
+%% tramite una assert(dishwasherEnabled(true))
+%% addTablewareDishwasher in dishwasherSupport.pl
+removeTablewareTable(T,N) :- tablewareTable(T,N1), N1 >= N, 
+							 retract(tablewareTable(T,N1)), N2 is N1 - N, 
+							 assert(tablewareTable(T,N2)),
+							 addTablewareDishwasher(T,N).
+
+%% non utilizzata, fa tutto randomConsumption in prepareFoodList.pl
+%% consuma randomicamente una certa quantit√† di ogni tableware
 %% rand_int genera un random tra 0 e N-1 inclusi
-randomTablewareConsumption :- tablewareTable(T,N), rand_int(N,R), R1 is R + 1, 
-						 	  retract(tablewareTable(T,N)), N1 is N - R1, 
-						 	  assert(tablewareTable(T,N1)), fail.
-randomTablewareConsumption.
+%%randomTablewareConsumption :- tablewareTable(T,N), N1 is N + 1, rand_int(N1,R), 
+%%						 	  retract(tablewareTable(T,N)), N2 is N - R, 
+%%						 	  assert(tablewareTable(T,N2)), fail.
+%%randomTablewareConsumption.
+
+%% per consumare i tableware corrispondenti al food appena consumato dalla
+%% randomFoodConsumption
+correspondingTablewareConsumption(C,N) :- corresponding(C,[H|T]), 
+										  listConsumption([H|T],N).
+
+%% per consumare ogni elemento della lista associata ad un particolare cibo 
+%% dal predicato corresponding
+listConsumption([],N).
+listConsumption([H],N) :- !, removeTablewareTable(H,N).
+listConsumption([H|T],N) :- removeTablewareTable(H,N), listConsumption(T,N).
 
 %% da provare dopo averla aggiornata con rand_int
 %%randomTablewareConsumption :- forall(tablewareTable(T,N),doRandomTablewareConsumption(T,N)).
