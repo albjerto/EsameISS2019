a65fda2633d9e3fced39cbbcccb64b20298d937e
imgiohunter <50119974+imgiohunter@users.noreply.github.com>
2019-11-01 16:30:25 +0100

    Update prepareTablewareList.pl

--- it.unibo.eclipse.qak.planning19/prepareTablewareList.pl
+++ it.unibo.eclipse.qak.planning19/prepareTablewareList.pl
@@ -1,21 +1,21 @@
 %% fatto da noi
 %% tableware da porre sul tavolo col comando prepare
-%% nome, quantità disponibile di ogni cibo visibile in pantryInit.pl
-%% (al momento presenti tutti i tableware, non in massima quantità)
-%% consigliabile modificare solo le quantità lasciando a 0 ciò che non si vuole prendere
+%% nome, quantitÃ  disponibile di ogni cibo visibile in pantryInit.pl
+%% (al momento presenti tutti i tableware, non in massima quantitÃ )
+%% consigliabile modificare solo le quantitÃ  lasciando a 0 ciÃ² che non si vuole prendere
 
-tablewarePrepare(piattoPiano,5).
-tablewarePrepare(ciotola,5).
-tablewarePrepare(bicchiere,5).
-tablewarePrepare(forchetta,5).
-tablewarePrepare(coltello,5).
-tablewarePrepare(cucchiaio,5).
+toPrepare(tableware(piattoPiano,5)).
+toPrepare(tableware(ciotola,5)).
+toPrepare(tableware(bicchiere,5)).
+toPrepare(tableware(forchetta,5)).
+toPrepare(tableware(coltello,5)).
+toPrepare(tableware(cucchiaio,5)).
 
 %% print dei tableware nella prepareTablewareList (NON stampa eventuali tableware che non si vogliono prendere)
-showPrepareTablewareList :- tablewarePrepare(T,N), N > 0, outputTablewarePrepare(T,N), fail.
+showPrepareTablewareList :- tableware(T,N), N > 0, outputTableware(T,N), fail.
 showPrepareTableware.
-outputTablewarePrepare(T,N) :- stdout <- print(T), stdout <- print(' '), stdout <- println(N).
+outputTableware(T,N) :- stdout <- print(T), stdout <- print(' '), stdout <- println(N).
 
 %% genera la stringa contenente la lista dei tableware richiesti (NON considera tableware che non si vogliono prendere) (da parsare lato kotlin)
-getPrepareTablewareList(L) :- findall([T,N], multipleGoal(T,N), L).
-multipleGoal(T,N) :- tablewarePrepare(T,N), N > 0.
+getPrepareTablewareList(L) :- findall(tableware(T,N), multipleGoal(T,N), L).
+multipleGoal(T,N) :- tableware(T,N), N > 0.
