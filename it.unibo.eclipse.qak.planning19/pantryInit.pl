dbd7041b6ea311e787df83e880a98676ffed4fe1
imgiohunter <50119974+imgiohunter@users.noreply.github.com>
2019-11-01 16:29:16 +0100

    Update pantryInit.pl

--- it.unibo.eclipse.qak.planning19/pantryInit.pl
+++ it.unibo.eclipse.qak.planning19/pantryInit.pl
@@ -1,28 +1,36 @@
 %% fatto da noi
 %% set di piatti, bicchieri e posate inizialmente disponibili nella pantry
 
-tableware(piattoPiano,20).
-tableware(ciotola,20).
-tableware(bicchiere,20).
-tableware(forchetta,20).
-tableware(coltello,20).
-tableare(cucchiaio,20).
+tableware(piattoPiano,20)
+tableware(ciotola,20)
+tableware(bicchiere,20)
+tableware(forchetta,20)
+tableware(coltello,20)
+tableare(cucchiaio,20)
 
-%% FUNZIONI TOMMY
-put([A|B]) :- assert(A), put(B).
-put([]).
-getFood([food(N,Q)|L]) :- getFoodByName(N,Q), getFood(L).
-getFood([]).
-getFoodByName(F,N) :- food(F,N1), N1 >= N, retract(food(F,N1)), N2 is N1 - N, assert(food(F,N2)).
+%% print dello stato (stampa anche tableware terminati)
+showPantryState :- tableware(T,N), outputTableware(T,N), fail.
+showPantryState.			
+outputTableware(T,N) :- stdout <- print(T), stdout <- print(' '), stdout <- println(N).
 
-getTableware([tableware(N,Q)|L]) :- getTablewareByName(N,Q), getTableware(L).
-getTableware([]).
-getTablewareByName(F,N) :- tableware(F,N1), N1 >= N, retract(tableware(F,N1)), N2 is N1 - N, assert(tableware(F,N2)).
-showFoodState(F) :- findall(food(T,N),food(T,N),F).
-showTableWareState(F) :- findall(tableware(T,N),tableware(T,N),F).
+%% genera la lista contenente lo stato della pantry (comprende eventuali tableware terminati) (da parsare lato kotlin)
+getPantryState(L) :- findall(tableware(T,N), tableware(T,N), L).
 
-remove([H|L]) :- retract(H), remove(L).
-remove([]).
+%% controllo presenza tableware con nome T
+isThereTableware(T) :- tableware(T,N), N > 0.
 
+%% rimozione dalla pantry dei tableware specificati nella lista passata come argomento (per la prepare)
+removeTablewareList([]).
+removeTablewareList([tableware(T,N)]) :- !, getTableware(T,N).
+removeTablewareList([tableware(T,N)|T1]) :- getTableware(T,N), removeTablewareList(T1).
 
+%% ritiro di una quantità N del tableware T
+getTableware(T,N) :- tableware(T,N1), N1 >= N, retract(tableware(T,N1)), N2 is N1 - N, assert(tableware(T,N2)).
 
+%% aggiunta alla pantry dei tableware specificati nella lista passata come argomento (per la clear)
+addTablewareList([]).
+addTablewareList([tableware(T,N)]) :- !, putTableware(T,N).
+addTablewareList([tableware(T,N)|T1]) :- putTableware(T,N), addTablewareList(T1).
+
+%% aggiunta di una quantità N del tableware T
+putTableware(T,N) :- tableware(T,N1), retract(tableware(T,N1)), N2 is N1 + N, assert(tableware(T,N2)).
