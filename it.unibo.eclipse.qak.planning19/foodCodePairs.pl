9d6314d21a4a6b171b2d27f1eabf0d49c9a6a590
imgiohunter <50119974+imgiohunter@users.noreply.github.com>
2019-11-06 22:03:06 +0100

    Prolog per coppie cibo-codice

---
+++ it.unibo.eclipse.qak.planning19/foodCodePairs.pl
@@ -0,0 +1,34 @@
+%% fatto da noi
+%% coppie cibo-codice per il passaggio dall'uno all'altro
+
+%% bevande
+pair(succoBuono,b01).
+pair(birraBella,b02).
+pair(acquaGelidaImbevibile,b03).
+pair(punchNeiDenti,b04).
+
+%% frutta
+pair(meloneVerdeImmaturo,f01).
+pair(peschePescate,f02).
+pair(meleMlem,f03).
+
+%% verdura
+pair(pomodoriComePatatine,v01).
+pair(caroteCheSuccedeAmico,v02).
+pair(ravanelliXtraSpicy,v03).
+
+%% affettato
+pair(salameGrassissimo,a01).
+pair(prosciuttoTroppoCostoso,a02).
+pair(coppaDeiCampioni,a03).
+pair(mortazzaDelVanish,a04).
+
+%% salato
+pair(pizzetteCompleanno,s01).
+pair(quelleRobePastaSfogliaEWurstel,s02).
+pair(piadaDelParco,s03).
+
+%% dolci
+pair(pannaCottaPoco,d01).
+pair(mascarponeDaMontagna,d02).
+pair(cheeseCakeHoFame,d03).
