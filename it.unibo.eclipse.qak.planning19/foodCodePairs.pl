65d34814c35706be505572e4e01d56f984f93c99
imgiohunter <50119974+imgiohunter@users.noreply.github.com>
2019-11-10 19:09:17 +0100

    Corretti codici

--- it.unibo.eclipse.qak.planning19/foodCodePairs.pl
+++ it.unibo.eclipse.qak.planning19/foodCodePairs.pl
@@ -3,32 +3,32 @@
 
 %% bevande
 pair(succoBuono,b01).
-pair(birraBella,b02).
-pair(acquaGelidaImbevibile,b03).
-pair(punchNeiDenti,b04).
+pair(acquaGelidaImbevibile,b02).
+%% pair(birraBella,b03).
+%% pair(punchNeiDenti,b04).
 
 %% frutta
-pair(meloneVerdeImmaturo,f01).
-pair(peschePescate,f02).
-pair(meleMlem,f03).
+pair(peschePescate,f01).
+pair(meleMlem,f02).
+%% pair(meloneVerdeImmaturo,f03).
 
 %% verdura
 pair(pomodoriComePatatine,v01).
 pair(caroteCheSuccedeAmico,v02).
-pair(ravanelliXtraSpicy,v03).
+%% pair(ravanelliXtraSpicy,v03).
 
 %% affettato
-pair(salameGrassissimo,a01).
-pair(prosciuttoTroppoCostoso,a02).
-pair(coppaDeiCampioni,a03).
-pair(mortazzaDelVanish,a04).
+pair(coppaDeiCampioni,a01).
+pair(mortazzaDelVanish,a02).
+%% pair(salameGrassissimo,a03).
+%% pair(prosciuttoTroppoCostoso,a04).
 
 %% salato
-pair(pizzetteCompleanno,s01).
-pair(quelleRobePastaSfogliaEWurstel,s02).
-pair(piadaDelParco,s03).
+pair(quelleRobePastaSfogliaEWurstel,s01).
+pair(piadaDelParco,s02).
+%% pair(pizzetteCompleanno,s03).
 
 %% dolci
 pair(pannaCottaPoco,d01).
-pair(mascarponeDaMontagna,d02).
-pair(cheeseCakeHoFame,d03).
+pair(cheeseCakeHoFame,d02).
+%% pair(mascarponeDaMontagna,d03).
